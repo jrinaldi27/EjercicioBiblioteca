c722c743ec987d6f9268d13ec39bd87ab254d2f9
Marcos Krimer <129324539+MarcosKrimer@users.noreply.github.com>
2023-06-07 18:56:13 -0300

    Update LibroNegocio.cs

--- EjercicioBiblioteca.CapaNegocio/LibroNegocio.cs
+++ EjercicioBiblioteca.CapaNegocio/LibroNegocio.cs
@@ -22,8 +22,21 @@ namespace EjercicioBiblioteca
             List<Libro> list = _libroDatos.TraerTodos();
 
             return list;
-        }
+        } 
+             public void Alta(string nombre, string apellido, string direccion, int idCliente, string email)
+             {
+                 Cliente cliente = new cliente();
+                 cliente.Nombre=nombre;
+                 cliente.Apellido=apellido;
+                 cliente.Direccion=direccion;
+                 cliente.IdCliente=idCliente;
+                 cliente.Email=email;
+                 
+                 TransactionResult transaction= _clienteDatos.Insertar(Cliente);
+                 
+                 if(!transaction.IsOk)
+                     throw new Exception(transaccion.Error);
+             }
 
-
-    }
+    } 
 }
