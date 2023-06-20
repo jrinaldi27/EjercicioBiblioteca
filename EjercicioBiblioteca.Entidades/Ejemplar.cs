53b2ab5749c5eda3b13dda900963d7f477df2f65
Marcos Krimer <129324539+MarcosKrimer@users.noreply.github.com>
2023-06-20 20:22:34 -0300

    Update Ejemplar.cs

--- EjercicioBiblioteca.Entidades/Ejemplar.cs
+++ EjercicioBiblioteca.Entidades/Ejemplar.cs
@@ -61,7 +61,7 @@ namespace EjercicioBiblioteca
 
         public override string ToString()
         {
-            return (this.IdLibro + ") " + this.Observaciones + ", " + this.FechaAlta + ", " + this.Id);
+            return ("Id Libro: " +this.IdLibro + ", " +  "Observaciones:" +this.Observaciones + ", " + "Fecha de Alta: " +  this.FechaAlta + ", " + "Id Ejemplar: " + this.Id);
         }
 
 
